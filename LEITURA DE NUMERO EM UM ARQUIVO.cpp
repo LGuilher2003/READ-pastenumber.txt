586219f54a9a9006b077fccf7057b47fb3d353db
Porco022 <111072200+Porco022@users.noreply.github.com>
2022-09-08 19:37:24 -0300

    Update LEITURA DE NUMERO EM UM ARQUIVO.cpp
    now if paste not exist, the programme inform

--- LEITURA DE NUMERO EM UM ARQUIVO.cpp
+++ LEITURA DE NUMERO EM UM ARQUIVO.cpp
@@ -2,8 +2,13 @@
 int main(void){
 	FILE *file;
 	int x,y,u;
-	//r-read(ler informações do arquivo,w-write(jogar um dado no arquivo),a-append
+	//r-read(ler informaÃ§Ãµes do arquivo,w-write(jogar um dado no arquivo),a-append
 	file= fopen("C:\\Users\\Guilherme\\Desktop\\teste.txt","r");
+	if(file == NULL){
+		printf("esse arquivo nao pode ser aberto\n");
+		
+		return 0;
+	}
 	fscanf(file,"%i %i %i", &x, &y, &u);
 	printf("%i %i %i", x,y,u);
 	fclose(file);
